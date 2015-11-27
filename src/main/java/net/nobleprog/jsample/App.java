6077193545e52e4dfc8c821e064229366d84a083
Lech Albrzykowski <la@pc5.home>
2015-11-27 17:57:44 +0100

    Initial commit

---
+++ src/main/java/net/nobleprog/jsample/App.java
@@ -0,0 +1,23 @@
+package net.nobleprog.jsample;
+
+/**
+ * Hello world.
+ *
+ */
+public final class App {
+    /**
+     * Class constructor.
+     */
+    private App() {
+    }
+    /**
+     * Application bootstrap.
+     *
+     * @param  args arguments list passed by user
+     */
+    //CHECKSTYLE:OFF
+    public static void main(String[] args) {
+    //CHECKSTYLE:ON
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/net/nobleprog/jsample/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Domain classes used to produce.
+ *
+ */
+package net.nobleprog.jsample;
