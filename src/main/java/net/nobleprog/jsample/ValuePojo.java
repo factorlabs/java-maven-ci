06028da1513298cb989b601656d954ae71384fba
Lech Albrzykowski <la@pc5.home>
2015-11-27 20:06:14 +0100

    adding failed code

---
+++ src/main/java/net/nobleprog/jsample/ValuePojo.java
@@ -0,0 +1,31 @@
+package net.nobleprog.jsample;
+
+/**
+ * High CC.
+ *
+ */
+public final class ValuePojo {
+    /**
+     * Class constructor.
+     */
+    private AValuePojo() {
+    }
+    /**
+     * Application bootstrap.
+     *
+     */
+    public static void foo() {
+      if (1==1) {
+        while (1) {
+          if (1 == 2) {
+
+          } else {
+            for (int i = 0; i<100; i++) {
+              if (true == true) {
+              }
+            }
+          }
+        }
+      }
+    }
+}
